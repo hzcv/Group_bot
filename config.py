5186cc8dc85e13bc25f223e0190148f7217c3421
Hrss <tackyverse5952@gmail.com>
2025-07-09 18:32:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ INSTAGRAM_PASSWORD = "your_ig_password"
 ADMIN_USERNAMES = ["admin1", "admin2"]  # lowercase IG usernames
 
 FIREBASE_CREDENTIAL_PATH = "path/to/your/firebase/serviceAccountKey.json"
-FIREBASE_DB_URL = "https://your-project.firebaseio.com/"
+FIREBASE_DB_URL = "https://instagram-bot-e355c-default-rtdb.firebaseio.com/"
 
 COOLDOWN_SECONDS = 10
