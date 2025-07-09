c025c28160749db7221ddb272bf45883e3d266d4
Hrss <tackyverse5952@gmail.com>
2025-07-09 18:24:06 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+INSTAGRAM_USERNAME = "your_ig_username"
+INSTAGRAM_PASSWORD = "your_ig_password"
+
+ADMIN_USERNAMES = ["admin1", "admin2"]  # lowercase IG usernames
+
+FIREBASE_CREDENTIAL_PATH = "path/to/your/firebase/serviceAccountKey.json"
+FIREBASE_DB_URL = "https://your-project.firebaseio.com/"
+
+COOLDOWN_SECONDS = 10
